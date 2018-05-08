f88c211ed176e56b78afa765ff4f6ba97f68ab7f
RandyCoffman <randy.coffman@wvjc.edu>
2018-05-08 12:30:19 -0400

    added tests to see if there is a winner and they are passing.

--- tictac_board.rb
+++ tictac_board.rb
@@ -1,11 +1,13 @@
 class Tictac_board
 
-    attr_accessor :board, :size
+    attr_accessor :board, :size, :win
 
     def initialize(size)
         @board = {}
         @size = size
+        @win = []
         board_generator()
+        win_generator()
     end
 
     def board_generator()
@@ -14,4 +16,29 @@ class Tictac_board
         end
     end
 
+    def win_generator() # Method that generates winning moves based on @size of the board
+
+        # Makes each possible win by using rows
+        for row in 1..@size do
+            @win<<((((row-1)*@size)+1)..(@size*row)).to_a
+        end
+
+        # Makes each possible win by using columns
+        for col in 1..@size do 
+            arr = Array.new
+            for num in 0..(@size-1) do 
+                arr.push((num*@size)+col)
+            end
+            @win<<arr
+        end
+
+        # Makes each possible win by using diagonals
+        for dia in [[1,(@size+1)],[@size,(@size-1)]] do
+            arr = Array.new
+            for num in 0..(@size-1) do
+                arr.push(dia[0]+(num*dia[1]))
+            end
+            @win<<arr
+        end
+    end
 end
--- tictac_board_tests.rb
+++ tictac_board_tests.rb
@@ -10,7 +10,6 @@ class Tictac_board_test < Minitest::Test
     def test_board_generator_9
         tictac = Tictac_board.new(3)
         board = tictac.board
-        p board
 
         assert_equal(9, board.length)
     end
@@ -28,4 +27,61 @@ class Tictac_board_test < Minitest::Test
 
         assert_equal(81, board.length)
     end
+
+    def test_for_winner_row_1
+        tictac = Tictac_board.new(3)
+        winner = tictac.win
+
+        assert_equal(winner[0], [1, 2, 3])
+    end
+
+    def test_for_winner_row_2
+        tictac = Tictac_board.new(3)
+        winner = tictac.win
+
+        assert_equal(winner[1], [4, 5, 6])
+    end
+
+    def test_for_winner_row_3
+        tictac = Tictac_board.new(3)
+        winner = tictac.win
+
+        assert_equal(winner[2], [7, 8, 9])
+    end
+
+    def test_for_winner_col_1
+        tictac = Tictac_board.new(3)
+        winner = tictac.win
+
+        assert_equal(winner[3], [1, 4, 7])
+    end
+
+    def test_for_winner_col_2
+        tictac = Tictac_board.new(3)
+        winner = tictac.win
+
+        assert_equal(winner[4], [2, 5, 8])
+    end
+
+    def test_for_winner_col_3
+        tictac = Tictac_board.new(3)
+        winner = tictac.win
+
+        assert_equal(winner[5], [3,6,9])
+    end
+
+    def test_for_winner_diag_1
+        tictac = Tictac_board.new(3)
+        winner = tictac.win
+
+        assert_equal(winner[6], [1, 5, 9])
+    end
+
+    def test_for_winner_diag_2
+        tictac = Tictac_board.new(3)
+        winner = tictac.win
+
+        assert_equal(winner[7], [3, 5, 7])
+    end
+
 end
