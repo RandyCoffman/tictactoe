8956876c3e68cd96ab6c4017ab2e4f1031183c9d
RandyCoffman <randy.coffman@wvjc.edu>
2018-05-08 12:00:51 -0400

    changed my class to make it more modular and tested for it and all tests are passing as expected.

--- tictac_board.rb
+++ tictac_board.rb
@@ -1,3 +1,17 @@
-def board
-	board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+class Tictac_board
+
+    attr_accessor :board, :size
+
+    def initialize(size)
+        @board = {}
+        @size = size
+        board_generator()
+    end
+
+    def board_generator()
+        for board_size in 1..(@size*@size) do
+                @board["#{board_size}"] = board_size
+        end
+    end
+
 end
--- tictac_board_tests.rb
+++ tictac_board_tests.rb
@@ -1,13 +1,31 @@
 require "minitest/autorun"
 require_relative "tictac_board.rb"
 
-class Tictac_board < Minitest::Test
+class Tictac_board_test < Minitest::Test
 
 	def test_true_equals_true		#This is made to pass so I can establish a base
 		assert_equal(true,true)
 	end
 
-	def test_board
-		assert_equal(9, board.size)
-	end
+    def test_board_generator_9
+        tictac = Tictac_board.new(3)
+        board = tictac.board
+        p board
+
+        assert_equal(9, board.length)
+    end
+
+    def test_board_generator_36
+        tictac = Tictac_board.new(6)
+        board = tictac.board
+
+        assert_equal(36, board.length)
+    end
+
+    def test_board_generator_81
+        tictac = Tictac_board.new(9)
+        board = tictac.board
+
+        assert_equal(81, board.length)
+    end
 end
