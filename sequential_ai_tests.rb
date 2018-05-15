a2845b741d99611135f41a0f41297269a7c2247a
RandyCoffman <randy.coffman@wvjc.edu>
2018-05-15 11:53:52 -0400

    changed a test variable just to see if the test still worked like it should and it's fully functional.

--- sequential_ai_tests.rb
+++ sequential_ai_tests.rb
@@ -17,35 +17,35 @@ class Tictac_sequential_ai_test < Minitest::Test
 	end
 
 	def test_sequential_ai_position_testing
-	sequential = Sequential_ai.new(3,"o")
-	board = Tictac_board.new(3)
-	testing = sequential.choice(board, "sequential_ai")
-	valid = board.valid_position?(testing)
+		sequential = Sequential_ai.new(3,"o")
+		board = Tictac_board.new(3)
+		testing = sequential.choice(board, "sequential_ai")
+		valid = board.valid_position?(testing)
 
-	assert_equal(true, valid)
+		assert_equal(true, valid)
 	end
 
 	def test_sequential_ai_position_testing2
-	sequential = Sequential_ai.new(4,"o")
-	board = Tictac_board.new(3)
-	board.board = {"1"=>"o","2"=>"x","3"=>"o","4"=>4,"5"=>"o","6"=>"x","7"=>7,"8"=>"x","9"=>9}
-	testing = sequential.choice(board, "sequential_ai")
-	p testing
-	valid = board.valid_position?(testing)
+		sequential = Sequential_ai.new(4,"o")
+		board = Tictac_board.new(3)
+		board.board = {"1"=>"o","2"=>"x","3"=>"o","4"=>4,"5"=>"o","6"=>"x","7"=>7,"8"=>"x","9"=>9}
+		testing = sequential.choice(board, "sequential_ai")
+		p testing
+		valid = board.valid_position?(testing)
 
-	assert_equal(true, valid)
+		assert_equal(true, valid)
 	end
 
 	def test_sequential_ai_position_testing3
-	sequential = Sequential_ai.new(4,"o")
-	board = Tictac_board.new(3)
-	board.board = {"1"=>"o","2"=>"x","3"=>"o","4"=>4,"5"=>"o","6"=>"x","7"=>7,"8"=>"x","9"=>9}
-	testing = sequential.choice(board, "sequential_ai")
-	p testing
-	valid = board.valid_position_input?(testing)
-	p valid
+		sequential = Sequential_ai.new(4,"o")
+		board = Tictac_board.new(3)
+		board.board = {"1"=>"o","2"=>"x","3"=>"o","4"=>"x","5"=>"o","6"=>"x","7"=>7,"8"=>"x","9"=>9}
+		testing = sequential.choice(board, "sequential_ai")
+		p testing
+		valid = board.valid_position_input?(testing)
+		p valid
 
-	assert_equal(true, valid)
+		assert_equal(true, valid)
 	end
 
 end
