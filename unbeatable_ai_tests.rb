2ba380df361c42641a4d571a047236c68d09ad95
RandyCoffman <randy.coffman@wvjc.edu>
2018-05-15 12:10:26 -0400

    I added a test to start as a baseline

---
+++ unbeatable_ai_tests.rb
@@ -0,0 +1,11 @@
+require "minitest/autorun"
+require_relative "unbeatable_ai.rb"
+require_relative "tictac_board.rb"
+
+class Tictac_unbeatable_ai_test < Minitest::Test
+
+	def test_true_equals_true		#This is made to pass so I can establish a base
+		assert_equal(true, true)
+	end
+
+end
