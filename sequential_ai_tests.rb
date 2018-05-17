eb43ca900f23708099a44061c138219315b1606e
RandyCoffman <randy.coffman@wvjc.edu>
2018-05-17 14:26:11 -0400

    saw a small error in my tests, which it wasn't hurting the testing, but I wanted to test the 3 by 3 size of my board instead of 4 by 4.

--- sequential_ai_tests.rb
+++ sequential_ai_tests.rb
@@ -26,7 +26,7 @@ class Tictac_sequential_ai_test < Minitest::Test
 	end
 
 	def test_sequential_ai_position_testing2
-		sequential = Sequential_ai.new(4,"o")
+		sequential = Sequential_ai.new(3,"o")
 		board = Tictac_board.new(3)
 		board.board = {"1"=>"o","2"=>"x","3"=>"o","4"=>4,"5"=>"o","6"=>"x","7"=>7,"8"=>"x","9"=>9}
 		testing = sequential.choice(board, "sequential_ai")
@@ -37,7 +37,7 @@ class Tictac_sequential_ai_test < Minitest::Test
 	end
 
 	def test_sequential_ai_position_testing3
-		sequential = Sequential_ai.new(4,"o")
+		sequential = Sequential_ai.new(3,"o")
 		board = Tictac_board.new(3)
 		board.board = {"1"=>"o","2"=>"x","3"=>"o","4"=>"x","5"=>"o","6"=>"x","7"=>7,"8"=>"x","9"=>9}
 		testing = sequential.choice(board, "sequential_ai")
