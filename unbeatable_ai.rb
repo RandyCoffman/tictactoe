fd0b3caede040d0fa9004f973ec219b4f7519e82
RandyCoffman <randy.coffman@wvjc.edu>
2018-05-17 14:26:30 -0400

    saw a small error in my tests, which it wasn't hurting the testing, but I wanted to test the 3 by 3 size of my board instead of 4 by 4.

--- unbeatable_ai.rb
+++ unbeatable_ai.rb
@@ -1,4 +1,5 @@
 require_relative "tictac_board.rb"
+require_relative "tictac_player.rb"
 
 class Unbeatable_ai
 
@@ -7,13 +8,22 @@ class Unbeatable_ai
     end
 
 	def choice(board_class,player)
+		# not putting anything in here until I get all my rules laid out and can figure out how it can make choices based on the rules
+	end
 
+	def winning_move(board_class,player)
+	  	moves_available(board_class).each { |winning_spot|
+	    	if board_class.winner_or_loser?()
+	    		p winning_spot
+	      		return winning_spot
+	    	end
+	  	}
 	end
 
 	def moves_available(board_class)
 	  my_array = []
-	  @board = board_class.board
-	  @board.each_pair do |key,value|
+	  board = board_class.board
+	  board.each_pair do |key,value|
 	      if value.class == Integer
 	          my_array.push(value)
 	      end
--- unbeatable_ai_tests.rb
+++ unbeatable_ai_tests.rb
@@ -1,6 +1,7 @@
 require "minitest/autorun"
 require_relative "unbeatable_ai.rb"
 require_relative "tictac_board.rb"
+require_relative "tictac_player.rb"
 
 class Tictac_unbeatable_ai_test < Minitest::Test
 
@@ -9,33 +10,34 @@ class Tictac_unbeatable_ai_test < Minitest::Test
 	end
 
 	def test_available_spots
-		unbeatable = Unbeatable_ai.new(4,"o")
+		unbeatable = Unbeatable_ai.new(3,"o")
 		board = Tictac_board.new(3)
 		board.board = {"1"=>"o","2"=>"x","3"=>"o","4"=>4,"5"=>"o","6"=>"x","7"=>7,"8"=>"x","9"=>9}
 		testing = unbeatable.moves_available(board)
-		p testing
+		# p testing
 
 		assert_equal([4, 7, 9], testing)
 	end
 
 	def test_available_spots2
-		unbeatable = Unbeatable_ai.new(4,"o")
+		unbeatable = Unbeatable_ai.new(3,"o")
 		board = Tictac_board.new(3)
 		board.board = {"1"=>"o","2"=>"x","3"=>3,"4"=>4,"5"=>"o","6"=>6,"7"=>7,"8"=>8,"9"=>9}
 		testing = unbeatable.moves_available(board)
-		p testing
+		# p testing
 
 		assert_equal([3, 4, 6, 7, 8, 9], testing)
 	end
 
 	def test_available_spots3
-		unbeatable = Unbeatable_ai.new(4,"o")
+		unbeatable = Unbeatable_ai.new(3,"o")
 		board = Tictac_board.new(3)
 		board.board = {"1"=>1,"2"=>2,"3"=>"o","4"=>4,"5"=>"o","6"=>"x","7"=>7,"8"=>"x","9"=>9}
 		testing = unbeatable.moves_available(board)
-		p testing
+		# p testing
 
 		assert_equal([1, 2, 4, 7, 9], testing)
 	end
+
 
 end
