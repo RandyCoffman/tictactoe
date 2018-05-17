f358388c7a1667dbec6561433b8db4ddc30be323
RandyCoffman <randy.coffman@wvjc.edu>
2018-05-17 15:54:30 -0400

    new function and tests made for it to see the position chosen

--- unbeatable_ai.rb
+++ unbeatable_ai.rb
@@ -8,27 +8,42 @@ class Unbeatable_ai
     end
 
 	def choice(board_class,player)
+		spot_chosen(board_class,player)
 		# not putting anything in here until I get all my rules laid out and can figure out how it can make choices based on the rules
 	end
 
-	def winning_move(board_class,player)
-	  	moves_available(board_class).each { |winning_spot|
-	    	if board_class.winner_or_loser?()
-	    		p winning_spot
-	      		return winning_spot
-	    	end
-	  	}
+	def spot_chosen(board_class,player)
+		board = board_class.board
+		winning_combo = board_class.win
+		x_array = []
+		o_array = []
+		winning_combo.each { |wins|
+			board.each_pair { |key,value|
+				if value == "x"
+					x_array.push(key.to_i)
+				elsif value == "o"
+					o_array.push(key.to_i)
+				end
+				x_array.uniq!
+				o_array.uniq!
+			}
+		}
+		if player.player == "o"
+			o_array
+		elsif player.player == "x"
+			x_array
+		end
 	end
 
 	def moves_available(board_class)
-	  my_array = []
-	  board = board_class.board
-	  board.each_pair do |key,value|
-	      if value.class == Integer
-	          my_array.push(value)
-	      end
-	  end
-	  my_array
+	  	my_array = []
+	  	board = board_class.board
+	  	board.each_pair { |key,value|
+	    	if value.class == Integer
+	        	my_array.push(value)
+	    	end
+	  	}
+	 	my_array
 	end
 
 
--- unbeatable_ai_tests.rb
+++ unbeatable_ai_tests.rb
@@ -39,5 +39,27 @@ class Tictac_unbeatable_ai_test < Minitest::Test
 		assert_equal([1, 2, 4, 7, 9], testing)
 	end
 
+	def test_chosen_spot
+		unbeatable = Unbeatable_ai.new(3,"o")
+		board = Tictac_board.new(3)
+		player = TicTac_player.new
+		player2 = player.change_icon
+		board.board = {"1"=>1,"2"=>2,"3"=>"o","4"=>4,"5"=>"o","6"=>"x","7"=>7,"8"=>"x","9"=>9}
+		testing = unbeatable.spot_chosen(board,player)
+		# p testing
+
+		assert_equal([3,5], testing)
+	end
+
+	def test_chosen_spot2
+		unbeatable = Unbeatable_ai.new(3,"x")
+		board = Tictac_board.new(3)
+		player = TicTac_player.new
+		board.board = {"1"=>1,"2"=>2,"3"=>"o","4"=>4,"5"=>"o","6"=>"x","7"=>7,"8"=>"x","9"=>9}
+		testing = unbeatable.spot_chosen(board,player)
+		# p testing
+
+		assert_equal([6,8], testing)
+	end
 
 end
