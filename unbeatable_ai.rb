0b7707fcc50a31886f95df3043cb798b8ec7678a
RandyCoffman <randy.coffman@wvjc.edu>
2018-05-21 12:11:41 -0400

    added two functions to check for a winning move and they are passing.

--- unbeatable_ai.rb
+++ unbeatable_ai.rb
@@ -8,8 +8,26 @@ class Unbeatable_ai
     end
 
 	def choice(board_class,player)
-		spot_chosen(board_class,player)
-		# not putting anything in here until I get all my rules laid out and can figure out how it can make choices based on the rules
+		p board_copy(board_class)
+		p board_class.win
+		p test_win_move(board_class,player.player,2)
+
+# not putting anything in here until I get all my rules laid out and can figure out how it can make choices based on the rules
+	end
+
+	def board_copy(board_class)
+# Making a duplicate of the board. When testing moves I don't want to change the actual board while testing
+	    board_class.board
+	end
+
+	def test_winning_move(board_class, marker, i)
+	    # b = the board
+	    # mark = 0 or X
+	    # i = the square to check if makes a win 
+	    b_copy = board_copy(board_class)
+	    b_copy[i] = marker
+	    board_class.update_board_with_position(marker,i)
+	    return board_class.win
 	end
 
 	def spot_chosen(board_class,player)
@@ -28,10 +46,11 @@ class Unbeatable_ai
 				o_array.uniq!
 			}
 		}
+		p player.player
 		if player.player == "o"
-			o_array
-		elsif player.player == "x"
 			x_array
+		elsif player.player == "x"
+			o_array
 		end
 	end
 
--- unbeatable_ai_tests.rb
+++ unbeatable_ai_tests.rb
@@ -48,7 +48,7 @@ class Tictac_unbeatable_ai_test < Minitest::Test
 		testing = unbeatable.spot_chosen(board,player)
 		# p testing
 
-		assert_equal([3,5], testing)
+		assert_equal([6,8], testing)
 	end
 
 	def test_chosen_spot2
@@ -59,7 +59,30 @@ class Tictac_unbeatable_ai_test < Minitest::Test
 		testing = unbeatable.spot_chosen(board,player)
 		# p testing
 
-		assert_equal([6,8], testing)
+		assert_equal([3,5], testing)
 	end
+
+	def test_copy_of_board
+		unbeatable = Unbeatable_ai.new(3,"x")
+		board = Tictac_board.new(3)
+		player = TicTac_player.new
+		testing = unbeatable.board_copy(board)
+		p testing
+
+		assert_equal({"1"=>1, "2"=>2, "3"=>3, "4"=>4, "5"=>5, "6"=>6, "7"=>7, "8"=>8, "9"=>9}, testing)
+	end
+
+	def test_the_winning_move
+		unbeatable = Unbeatable_ai.new(3,"x")
+		board = Tictac_board.new(3)
+		board.board = {"1"=>1,"2"=>2,"3"=>"o","4"=>4,"5"=>"o","6"=>"x","7"=>7,"8"=>"x","9"=>"x"}
+		player = TicTac_player.new
+		testing = unbeatable.test_winning_move(board, player.player, 7)
+
+		assert_equal([[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]], testing)
+	end
+	123
+	456
+	789
 
 end
