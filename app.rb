7c6aae9b072dd260795d8a5760122f65465e7081
RandyCoffman <randy.coffman@wvjc.edu>
2018-05-31 13:48:42 -0400

    added second page

--- app.rb
+++ app.rb
@@ -1,4 +1,10 @@
 require 'sinatra'
+require_relative "random_ai.rb"
+require_relative "sequential_ai.rb"
+require_relative "unbeatable_ai.rb"
+require_relative "tictac_board.rb"
+require_relative "tictac_player.rb"
+
 
 enable :sessions
 
@@ -8,6 +14,17 @@ end
 
 post "/post-size" do
 	session[:size] = params[:size]
-	@size = session[:size]
-	p @size
+	size = session[:size].to_i
+	redirect "/player-selection"
+end
+
+get "/player-selection" do
+	erb :page2_player_selection
+end
+
+post "/post-player-selection" do
+	session[:players] = params[:players]
+	session[:p1_marker] = params[:p1_marker]
+	size = session[:size]
+	p session[:p1_marker]
 end
