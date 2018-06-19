87d757a97911ebf948f1bef1f89164eb685419d9
RandyCoffman <randy.coffman@wvjc.edu>
2018-06-19 11:42:50 -0400

    final

--- unbeatable_ai.rb
+++ unbeatable_ai.rb
@@ -209,7 +209,7 @@ class Unbeatable_ai
 				# p ""
 				my_fork = each_element - matches
 				# p my_fork
-				if my_fork.count == @size - 1
+				if my_fork.count == @size - 2
 					done = my_fork & get_side_positions(board_class)
 					while board_class.valid_position?(done[counter]) != true
 						counter = counter + 1
@@ -228,7 +228,7 @@ class Unbeatable_ai
 			for each_element in board_class.win
 				matches = each_element & spot_chosen_by_o(board_class)
 				my_fork = each_element - matches
-				if my_fork.count == @size - 1
+				if my_fork.count == @size - 2
 					done = my_fork & get_side_positions(board_class)
 					while board_class.valid_position?(done[counter]) != true
 						counter = counter + 1
