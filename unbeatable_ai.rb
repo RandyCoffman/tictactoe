6ca1cc48295149fa3c3f379c07d42e8ce169cebf
RandyCoffman <randy.coffman@wvjc.edu>
2018-06-19 15:13:14 -0400

    small fix, but wasn't having a problem with it

--- unbeatable_ai.rb
+++ unbeatable_ai.rb
@@ -204,8 +204,14 @@ class Unbeatable_ai
 		fork_array = []
 		counter = 0
 		# p spot_chosen_by_x(board_class).count
-		if spot_chosen_by_x(board_class) == [@size-1,@size+1] || spot_chosen_by_x(board_class) == [@size+1,@size-1]
+		if spot_chosen_by_x(board_class) == [2,@size+1] || spot_chosen_by_x(board_class) == [@size+1,2]
 			return 1
+		elsif spot_chosen_by_x(board_class) == [@size-1,@size+@size] || spot_chosen_by_x(board_class) == [@size+@size,@size-1]
+			return @size
+		elsif spot_chosen_by_x(board_class) == [@size*(@size-1)+2,@size*(@size-1)-2] || spot_chosen_by_x(board_class) == [@size*(@size-1)-2,@size*(@size-1)+2]
+			return @size*(@size-1)+1
+		elsif spot_chosen_by_x(board_class) == [@size*(@size-1),@size*(@size-1)+2] || spot_chosen_by_x(board_class) == [@size*(@size-1)+2,@size*(@size-1)]
+			return @size**@size
 		end
 		if spot_chosen_by_x(board_class).count > 1
 			for each_element in board_class.win
@@ -231,6 +237,12 @@ class Unbeatable_ai
 		counter = 0
 		if spot_chosen_by_o(board_class) == [2,@size+1] || spot_chosen_by_o(board_class) == [@size+1,2]
 			return 1
+		elsif spot_chosen_by_o(board_class) == [@size-1,@size+@size] || spot_chosen_by_o(board_class) == [@size+@size,@size-1]
+			return @size
+		elsif spot_chosen_by_o(board_class) == [@size*(@size-1)+2,@size*(@size-1)-2] || spot_chosen_by_o(board_class) == [@size*(@size-1)-2,@size*(@size-1)+2]
+			return @size*(@size-1)+1
+		elsif spot_chosen_by_o(board_class) == [@size*(@size-1),@size*(@size-1)+2] || spot_chosen_by_o(board_class) == [@size*(@size-1)+2,@size*(@size-1)]
+			return @size**@size
 		end
 		if spot_chosen_by_o(board_class).count > 1
 			for each_element in board_class.win
