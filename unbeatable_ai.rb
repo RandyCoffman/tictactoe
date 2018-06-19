386fce386738f2ba7cf1ba8435ca92d5c071773f
RandyCoffman <randy.coffman@wvjc.edu>
2018-06-19 11:33:38 -0400

    final

--- unbeatable_ai.rb
+++ unbeatable_ai.rb
@@ -159,9 +159,8 @@ class Unbeatable_ai
 	def take_win_if_available_player_x(board_class)
 		for each_element in board_class.win
 			x_matches = each_element & spot_chosen_by_x(board_class)
-			if x_matches.count == @size - 2
-				x_win = each_element - spot_chosen_by_o(board_class)
-				x_win = each_element - x_matches
+			if x_matches.count == @size - 1
+				x_win = x_matches - spot_chosen_by_o(board_class)
 				if board_class.valid_position?(x_win.join.to_i) == true
 					return x_win.join.to_i
 				end
@@ -173,9 +172,8 @@ class Unbeatable_ai
 	def take_win_if_available_player_o(board_class)
 		for each_element in board_class.win
 			o_matches = each_element & spot_chosen_by_o(board_class)
-			if o_matches.count == @size - 2
-				o_win = each_element - spot_chosen_by_x(board_class)
-				o_win = each_element - o_matches
+			if o_matches.count == @size - 1
+				o_win = o_matches - spot_chosen_by_x(board_class)
 				if board_class.valid_position?(o_win.join.to_i) == true
 					return o_win.join.to_i
 				end
